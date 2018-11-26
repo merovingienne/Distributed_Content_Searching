c3dcf2d8f6992122e28e96fb48d021f473803412
lahiru-j <lahirujayathilake@gmail.com>
2018-11-26 19:19:50 +0530

    File implementation

---
+++ content-searching/src/main/java/org/altumtek/filetransfer/DFile.java
@@ -0,0 +1,50 @@
+package org.altumtek.filetransfer;
+
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
+import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DFile {
+
+    public File createFile(String name) {
+        String fileName = System.getProperty("java.io.tmpdir") + File.separator + name;
+        // Generate a random number between 2-10
+        int fileSizeInBytes = 1024 * 1024 * ThreadLocalRandom.current().nextInt(2, 10 + 1);
+        char[] chars = new char[fileSizeInBytes];
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+            bw.write(chars);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        File file = new File(fileName);
+        if (file.exists()) {
+            try {
+                System.out.printf("%s File Size = %.2f MB\n", name, (file.length() / (1024 * 1024)) * 1.0); //FIXME not correctly getting the size (decimal part)
+                System.out.printf("SHA of the %s File = %s\n", name, sha1(file));
+            } catch (NoSuchAlgorithmException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return file;
+    }
+
+    private String sha1(File file) throws NoSuchAlgorithmException, IOException {
+        MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
+        InputStream fis = new FileInputStream(file);
+        int n = 0;
+        byte[] buffer = new byte[8192];
+        while (n != -1) {
+            n = fis.read(buffer);
+            if (n > 0) {
+                sha1.update(buffer, 0, n);
+            }
+        }
+
+        return new HexBinaryAdapter().marshal(sha1.digest());
+    }
+}
