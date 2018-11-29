3d85c079fe6ade71d854fb4709c1edbb0cb17a82
lahiru-j <lahirujayathilake@gmail.com>
2018-11-30 01:38:44 +0530

    updated the file client

--- content-searching/src/main/java/org/altumtek/filetransfer/FileClient.java
+++ content-searching/src/main/java/org/altumtek/filetransfer/FileClient.java
@@ -1,6 +1,7 @@
 package org.altumtek.filetransfer;
 
 import org.altumtek.filemanager.FileManager;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -9,6 +10,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.log4j.Logger;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.Arrays;
 
@@ -22,18 +24,40 @@ public class FileClient {
 
     public static boolean download(String ipAddr, int port, String fileName) {
         if (Arrays.asList(FileManager.getFiles()).contains(fileName)) {
-            File file = new File(fileName);
+            File file = new File(fileName.replace("-", " "));
+
             CloseableHttpClient client = HttpClients.createDefault();
             try (CloseableHttpResponse response = client.execute(new HttpGet("http://" + ipAddr + ":" + port + PATH + File.separator + fileName))) {
-                logger.info("Request received from the node " + ipAddr + " : " + port + "Ready to download the file: " + fileName);
+                logger.info("FILE DOWNLOAD: Request received from the node " + ipAddr + " : " + port + "Ready to download the file: " + fileName);
+
                 HttpEntity entity = response.getEntity();
                 if (entity != null) {
                     try (FileOutputStream outStream = new FileOutputStream(file)) {
                         entity.writeTo(outStream);
                     }
+
+                } else {
+                    logger.error("FILE DOWNLOAD: Download Failed! Response body is empty in the file: " + fileName + " which downloaded from: " + ipAddr + " : " + port);
+                    return false;
                 }
-                logger.info("Successfully sent the file: " + fileName + " to: " + ipAddr + " : " + port);
-                return true;
+                logger.info("FILE DOWNLOAD: File: " + fileName + " downloaded successfully!");
+
+                logger.info("FILE DOWNLOAD: Start verifying the content...");
+                String sha1Received = (response.getHeaders("Checksum-SHA1"))[0].toString().split(" ")[1];
+                logger.info("FILE DOWNLOAD: Received hash value: " + sha1Received);
+
+                String sha1Generated = DigestUtils.sha1Hex(new FileInputStream(file)).toUpperCase();
+                logger.info("FILE DOWNLOAD: Generated hash value: " + sha1Generated);
+                String fileSize = (response.getHeaders("Content-Length"))[0].toString();
+
+                if (sha1Generated.equals(sha1Received)) {
+                    logger.info("FILE DOWNLOAD: Validation successful! Both hashed values match! Downloaded file size: " + fileSize);
+                    return true;
+
+                } else {
+                    logger.error("FILE DOWNLOAD: Validation failed! hashed values do not match!");
+                    return false;
+                }
 
             } catch (Exception e) {
                 logger.error("Exception occurred while downloading the file: " + fileName +
